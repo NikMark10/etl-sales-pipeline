589260e249810018fa6939c227e0fe02b3d03139
Nikhil Kumar Pal <inikhil347@gmail.com>
2026-01-04 12:32:11 +0000

    Sync WSL changes

--- run_etl.py
+++ run_etl.py
@@ -5,6 +5,7 @@ import logging
 import os
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
+from datetime import datetime
 load_dotenv()
 
 user = os.getenv("DB_USER")
@@ -15,6 +16,12 @@ db = os.getenv("DB_NAME")
 
 # making sure logs directory exists
 os.makedirs("logs", exist_ok=True)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+RAW_FILE_PATH = os.path.join(
+    BASE_DIR, "data", "raw", "sales_raw.csv"
+)
 
 logging.basicConfig(
     filename="logs/etl.log",
@@ -27,7 +34,7 @@ DB_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 def main():
     logging.info("ETL job started")
 
-    df = extract_sales_data("data/raw/sales_raw.csv")
+    df = extract_sales_data(RAW_FILE_PATH)
     logging.info(f"Extracted {len(df)} rows")
 
     df_clean = transform_sales_data(df)
@@ -35,6 +42,10 @@ def main():
 
     load_to_db(df_clean, DB_URL)
     logging.info("ETL job completed successfully}")
+
+    # --- HEARTBEAT LOG ---
+    with open("/home/inikh/projects/etl-sales-pipeline/logs/etl.log", "a") as f:
+        f.write(f"ETL ran successfully at {datetime.now()}\n")
 
 if __name__ == "__main__":
     main()
